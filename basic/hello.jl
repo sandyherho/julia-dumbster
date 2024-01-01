ecc6e262c659e43e00f1a5033351b1f00a3a165e
dipa <dipa@Nusantara.ucr.edu>
2024-01-01 08:25:29 -0800

    first commit

---
+++ basic/hello.jl
@@ -0,0 +1,41 @@
+### A Pluto.jl notebook ###
+# v0.19.36
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 73ea805a-a80b-11ee-2b2c-a38f6e387087
+s1 = "Hello ";
+
+# ╔═╡ c5457000-8349-4214-98fc-75f2ab409651
+s2 = " World!";
+
+# ╔═╡ 262d4ee9-623b-4fea-87b4-2a826a2378fe
+print(s1)
+
+# ╔═╡ 72ee7ce7-4eae-486b-a5ad-2e7404588116
+print(s2)
+
+# ╔═╡ 83bca808-e3a5-41cb-8023-d28ddb890bef
+println(s1)
+
+# ╔═╡ 75f71921-8413-4880-9b1c-38bbc24b9bfd
+3 + 5
+
+# ╔═╡ e540d425-c74a-4f7a-a1dd-a73c02911a99
+(3 + 4 + 9)/
+	(15 - 20)-
+	(12 * 3)
+
+# ╔═╡ ea7c7a70-8195-4377-af56-2cc4ff154db1
+
+
+# ╔═╡ Cell order:
+# ╠═73ea805a-a80b-11ee-2b2c-a38f6e387087
+# ╠═c5457000-8349-4214-98fc-75f2ab409651
+# ╠═262d4ee9-623b-4fea-87b4-2a826a2378fe
+# ╠═72ee7ce7-4eae-486b-a5ad-2e7404588116
+# ╠═83bca808-e3a5-41cb-8023-d28ddb890bef
+# ╠═75f71921-8413-4880-9b1c-38bbc24b9bfd
+# ╠═e540d425-c74a-4f7a-a1dd-a73c02911a99
+# ╠═ea7c7a70-8195-4377-af56-2cc4ff154db1
